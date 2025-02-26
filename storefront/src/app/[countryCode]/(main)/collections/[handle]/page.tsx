9e6dc17ed5ecfacb34e1e531279d608b63f9f21c
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-02-26 07:32:55 -0800

    feat: Updated metadata to reflect House of Joseph Lynch.

--- storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/collections/[handle]/page.tsx
@@ -59,7 +59,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 
   const metadata = {
-    title: `${collection.title} | Medusa Store`,
+    title: `${collection.title} | House of Joseph Lynch`,
     description: `${collection.title} collection`,
   } as Metadata
 
