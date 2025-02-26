b7a3d2aeda2ea08aa6b9eab6be6b08ebc488fccc
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-02-26 10:36:04 -0800

    feat: Altered pictures to fix height issue.2.0

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -10,9 +10,9 @@ export default async function Nav() {
   const regions = await listRegions().then((regions: StoreRegion[]) => regions)
 
   return (
-    <div className="sticky top-0 inset-x-0 z-50 group">
-      <header className="relative h-16 mx-auto border-b duration-200 bg-white border-ui-border-base">
-        <nav className="content-container txt-xsmall-plus text-ui-fg-subtle flex items-center justify-between w-full h-full text-small-regular">
+    <div className="fixed top-0 inset-x-0 z-50 group">
+      <header className="relative h-16 mx-auto duration-200 bg-transparent">
+        <nav className="content-container txt-xsmall-plus text-white flex items-center justify-between w-full h-full text-small-regular">
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">
               <SideMenu regions={regions} />
