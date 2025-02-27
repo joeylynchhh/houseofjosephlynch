38af308fff0c7f2af935e733086e30f35cc16c73
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-02-27 11:13:02 -0800

    Altering the UI of the homepage to remove whitespace.7.0

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -4,17 +4,17 @@ import LocalizedClientLink from "@modules/common/components/localized-client-lin
 
 const Hero = () => {
   return (
-    <div className="h-[88vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle overflow-hidden">
+    <div className="h-screen w-full border-b border-ui-border-base relative bg-ui-bg-subtle overflow-hidden">
       {/* Background images container */}
-      <div className="absolute inset-0 w-full h-[112%] flex z-0">
+      <div className="absolute inset-0 w-full h-full flex z-0">
         <LocalizedClientLink 
           href="/womens"
           className="w-1/2 h-full relative group cursor-pointer z-[1] overflow-hidden"
         >
           <div 
-            className="w-full h-full bg-cover bg-center transition-transform duration-300 group-hover:scale-105"
+            className="w-full h-[112%] bg-cover bg-center transition-transform duration-300 group-hover:scale-105"
             style={{
-              backgroundImage: "url('/ZIPPER4504_fd50cec1-1eb8-4150-b158-1a6a3c4dd625.png.webp')",
+              backgroundImage: "url('/femalemodel1.png.webp')",
             }}
           >
             <div className="absolute bottom-0 left-0 right-0 h-full bg-gradient-to-b from-transparent to-black/50">
@@ -30,9 +30,9 @@ const Hero = () => {
           className="w-1/2 h-full relative group cursor-pointer z-[1] overflow-hidden"
         >
           <div 
-            className="w-full h-full bg-cover bg-center transition-transform duration-300 group-hover:scale-105"
+            className="w-full h-[112%] bg-cover bg-center transition-transform duration-300 group-hover:scale-105"
             style={{
-              backgroundImage: "url('/1000GSMHOODIE1.png.webp')",
+              backgroundImage: "url('/malemodel1.png.webp')",
             }}
           >
             <div className="absolute bottom-0 left-0 right-0 h-full bg-gradient-to-b from-transparent to-black/50">
