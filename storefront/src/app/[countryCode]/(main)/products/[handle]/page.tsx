671478836e3e84b9c037d1526fd746f3a8e684cb
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-02-28 09:37:01 -0800

    Altering the UI of the header on the products and store pages.

--- storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ storefront/src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from "next"
 import { notFound } from "next/navigation"
+import Nav from "@modules/layout/templates/nav"
 
 import ProductTemplate from "@modules/products/templates"
 import { getRegion, listRegions } from "@lib/data/regions"
@@ -80,10 +81,15 @@ export default async function ProductPage({ params }: Props) {
   }
 
   return (
-    <ProductTemplate
-      product={pricedProduct}
-      region={region}
-      countryCode={params.countryCode}
-    />
+    <>
+      <Nav />
+      <div className="pt-24">
+        <ProductTemplate
+          product={pricedProduct}
+          region={region}
+          countryCode={params.countryCode}
+        />
+      </div>
+    </>
   )
 }
--- storefront/src/app/[countryCode]/(main)/store/page.tsx
+++ storefront/src/app/[countryCode]/(main)/store/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next"
-
+import Nav from "@modules/layout/templates/nav"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 import StoreTemplate from "@modules/store/templates"
 
@@ -22,10 +22,15 @@ export default async function StorePage({ searchParams, params }: Params) {
   const { sortBy, page } = searchParams
 
   return (
-    <StoreTemplate
-      sortBy={sortBy}
-      page={page}
-      countryCode={params.countryCode}
-    />
+    <>
+      <Nav />
+      <div className="pt-24">
+        <StoreTemplate
+          sortBy={sortBy}
+          page={page}
+          countryCode={params.countryCode}
+        />
+      </div>
+    </>
   )
 }
