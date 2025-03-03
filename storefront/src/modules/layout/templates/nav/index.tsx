9cea60433d7454534f72fcdd9b581070a94cb9a8
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-03-03 03:54:22 -0800

    Altering the UI of the header to fix a typo.

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -15,8 +15,8 @@ export default async function Nav() {
 
   return (
     <div className="fixed top-0 inset-x-0 z-50">
-      <header className={`relative h-16 mx-auto duration-200 ${isHomePage ? 'bg-transparent hover:bg-white' : 'bg-white'}`}>
-        <nav className={`content-container txt-xsmall-plus flex items-center justify-between w-full h-full text-small-regular ${isHomePage ? 'text-white hover:text-black' : 'text-black'}`}>
+      <header className={`relative h-16 mx-auto duration-200 ${isHomePage ? 'bg-transparent' : 'bg-white'}`}>
+        <nav className={`content-container txt-xsmall-plus flex items-center justify-between w-full h-full text-small-regular ${isHomePage ? 'text-white' : 'text-black'}`}>
           <div className="flex-1 basis-0 h-full flex items-center">
             <div className="h-full">
               <SideMenu regions={regions} />
