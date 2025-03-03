1777ebf7f4fafa0339996d0dd66abd64e47008a3
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-03-03 03:47:43 -0800

    Altering the UI of the homepage to fix interactive images.

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -7,9 +7,9 @@ const Hero = () => {
     <div className="h-screen w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
       {/* Background images container */}
       <div className="absolute inset-0 w-full h-full flex z-0">
-        <LocalizedClientLink 
+        <a 
           href="/womens"
-          className="w-1/2 h-full relative group cursor-pointer z-[1]"
+          className="w-1/2 h-full relative group cursor-pointer z-[1] block"
         >
           <div 
             className="w-full h-full bg-cover bg-center transition-transform duration-300 group-hover:scale-105"
@@ -19,15 +19,15 @@ const Hero = () => {
           >
             <div className="absolute bottom-0 left-0 right-0 h-full bg-gradient-to-b from-transparent to-black/50">
               <div className="absolute bottom-8 left-8">
-                <span className="text-white text-3xl font-light pointer-events-none">Women</span>
+                <span className="text-white text-3xl font-light">Women</span>
               </div>
             </div>
           </div>
-        </LocalizedClientLink>
+        </a>
 
-        <LocalizedClientLink 
+        <a 
           href="/mens"
-          className="w-1/2 h-full relative group cursor-pointer z-[1]"
+          className="w-1/2 h-full relative group cursor-pointer z-[1] block"
         >
           <div 
             className="w-full h-full bg-cover bg-center transition-transform duration-300 group-hover:scale-105"
@@ -37,11 +37,11 @@ const Hero = () => {
           >
             <div className="absolute bottom-0 left-0 right-0 h-full bg-gradient-to-b from-transparent to-black/50">
               <div className="absolute bottom-8 left-8">
-                <span className="text-white text-3xl font-light pointer-events-none">Men</span>
+                <span className="text-white text-3xl font-light">Men</span>
               </div>
             </div>
           </div>
-        </LocalizedClientLink>
+        </a>
       </div>
 
       {/* Existing content overlay */}
