b471b7c2bc317668fe2def323e9392871d97b330
joeylynchhh <joseph.lynch.dr@gmail.com>
2025-03-04 04:54:11 -0800

    Altering the UI of the store to ensure fix the nav.

--- storefront/src/app/[countryCode]/(main)/store/page.tsx
+++ storefront/src/app/[countryCode]/(main)/store/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next"
-import Nav from "@modules/layout/templates/nav"
 import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 import StoreTemplate from "@modules/store/templates"
 
@@ -22,15 +21,12 @@ export default async function StorePage({ searchParams, params }: Params) {
   const { sortBy, page } = searchParams
 
   return (
-    <>
-      <Nav />
-      <div className="pt-24">
-        <StoreTemplate
-          sortBy={sortBy}
-          page={page}
-          countryCode={params.countryCode}
-        />
-      </div>
-    </>
+    <div className="pt-24">
+      <StoreTemplate
+        sortBy={sortBy}
+        page={page}
+        countryCode={params.countryCode}
+      />
+    </div>
   )
 }
